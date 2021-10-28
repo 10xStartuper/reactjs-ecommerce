e892701550875a299e5a33c377468615b94a1613
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-29 02:42:07 +0500

    eCommerce-UI Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,87 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCart } from "@material-ui/icons";
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 64px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+//Left side of a Navbar
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid #333;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
+  outline: none;
+`;
+
+//Central part of ne Navbar goes here
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h2``;
+
+//Right
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  text-transform: uppercase;
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search here" />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>TechBook.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>Sign Up</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCart />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
