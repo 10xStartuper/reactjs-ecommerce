43ac01ec1a9e580f5bd920b333ddc9ee4be7497b
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-29 01:35:05 +0500

    index.css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
+// import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello world</header>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <>Hey this is home</>;
+};
+
+export default Home;
