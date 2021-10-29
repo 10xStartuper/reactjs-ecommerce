2a7c797dd567c2122299aec9244450c2101a215d
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-30 03:22:20 +0500

    Slider ready

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -33,7 +33,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transition: all 1.5s ease;
+  transition: all 1s ease-in-out;
   transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 const Slide = styled.div`
--- src/data.js
+++ src/data.js
@@ -8,14 +8,14 @@ const sliderItems = [
   },
   {
     id: 2,
-    img: "https://i.ibb.co/DG69bQ4/2.png",
+    img: "https://www.makemagazine.co.uk/wp-content/uploads/2021/05/thumbnail-2.jpeg",
     title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://i.ibb.co/cXFnLLV/3.png",
+    img: "https://image.isu.pub/210513211153-e445d5423a445b61dedbb5fe479fefd7/jpg/page_1_thumb_large.jpg",
     title: "LOUNGEWEAR LOVE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
