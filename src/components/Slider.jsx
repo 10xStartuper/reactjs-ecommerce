658f0f0bc08bcceba334f337b6f03609ddb81f8a
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-30 03:18:16 +0500

    Slider ready

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,41 +1,115 @@
 import { IconButton } from "@material-ui/core";
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import sliderItems from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #ececec;
+  background-color: #fff7f7;
+  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
   position: absolute;
   top: 0;
   bottom: 0;
-  margin: auto;
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  /* opacity: 0.5; */
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  height: 80%;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Image = styled.img`
+  height: 100%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+`;
+const Button = styled.button`
+  outline: none;
+  border: none;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
   cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <IconButton>
           <ArrowBackIos />
         </IconButton>
       </Arrow>
-
-      <Arrow direction="right">
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((el) => {
+          return (
+            <Slide bg={el.bg} key={el.id}>
+              <ImgContainer>
+                <Image src={el.img} />
+              </ImgContainer>
+              <InfoContainer>
+                <Title>{el.title}</Title>
+                <Description>{el.desc}</Description>
+                <Button>BUY NOW</Button>
+              </InfoContainer>
+            </Slide>
+          );
+        })}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <IconButton>
           <ArrowForwardIos />
         </IconButton>
