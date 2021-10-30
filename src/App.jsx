f3dbefd1a40f396f71f1af36de6fc75f26ec912e
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-30 09:30:00 +0500

    Auth pages done)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+// import Login from "./pages/Login";
+
+import Register from "./pages/Register";
+
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Register />
     </div>
   );
 }
